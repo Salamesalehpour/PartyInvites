0e596984ad3a227c68c592bf62c150d46a443850
Salam Salehpour <SalameSalehpour@gmail.com>
2021-03-11 17:15:07 +0330

    Creating a Second Action and View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,5 +15,10 @@ namespace PartyInvites.Controllers
         {
             return View();
         }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
     }
 }
